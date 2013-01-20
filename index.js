76f44e3fda4dcb01875c221442d997cd65aafce9
jkroso <jkroso@gmail.com>
2013-01-21 10:09:07 +1300

    removed dependency

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
-var contains = require('contains')
-
 /**
- * Returns a new array with elements filtered by uniqueness
- *
+ * Returns a new array without duplicate elements
+ * 
  * @param {Array} arr
  * @return {Array}
  */
 
 module.exports = function (arr) {
-  var result = []
-    , len = arr.length
+  var len = arr.length
+  if (!len) return []
 
-  if (!len) return result
-  result.push(arr[0])
+  var result = [arr[0]]
+    , rc = 1
+    , i = 1
 
-  for (var i = 1; i < len; ++i) {
-    var el = arr[i]
+  each: while (i < len) {
+    var el = arr[i++]
+      , c = 0
 
-    if (!contains(el, result)) {
-      result.push(el)
+    while (c < rc) {
+      if (result[c++] === el) continue each
     }
+
+    result[rc++] = el
   }
 
   return result
